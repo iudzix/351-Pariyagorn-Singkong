639ae82707bb3d1143667483b9f690d7eecddf6c
iudzix <pariygpariyagorn@gmail.com>
2025-10-08 10:16:32 +0700

    CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -48,7 +48,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+       publicPath: '/351-Pariyagorn-Singkong/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
